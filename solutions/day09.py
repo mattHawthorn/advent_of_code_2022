5e421a2e8fc65a3f42a2a8e73eb8b6df3c3f8025
matt.hawthorn <matt.hawthorn@trillianthealth.com>
2022-12-11 12:21:56 -0500

    day 9 solution part 1

---
+++ solutions/day09.py
@@ -0,0 +1,95 @@
+from operator import itemgetter
+from typing import IO, Dict, Iterable, Iterator, NewType, Tuple, cast
+
+from .util import print_, set_verbose
+
+Direction = NewType("Direction", str)
+Distance = NewType("Distance", int)
+Coord = NewType("Coord", int)
+Instruction = Tuple[Direction, Distance]
+Position = Tuple[Coord, Coord]
+Vector = Tuple[Distance, Distance]
+State = Tuple[Position, Position]
+
+R, L, U, D = Direction("R"), Direction("L"), Direction("U"), Direction("D")
+STEP_VECTORS = cast(
+    Dict[Direction, Vector],
+    {R: (1, 0), L: (-1, 0), U: (0, 1), D: (0, -1)},
+)
+
+
+def parse_instruction(line: str) -> Instruction:
+    direction, dist = line.rstrip().split(maxsplit=1)
+    return Direction(direction), Distance(int(dist))
+
+
+def move(pos: Position, vec: Vector) -> Position:
+    return Coord(pos[0] + vec[0]), Coord(pos[1] + vec[1])
+
+
+def sign(x: int) -> int:
+    return 0 if x == 0 else (1 if x > 0 else -1)
+
+
+def step_dist(distance: Distance) -> Distance:
+    return Distance(min(abs(distance), 1) * sign(distance))
+
+
+def catch_up(head: Position, tail: Position) -> Position:
+    hx, hy = head
+    tx, ty = tail
+    vec = vx, vy = Distance(hx - tx), Distance(hy - ty)
+    if max(map(abs, vec)) == 1:  # type: ignore
+        return tail
+    else:
+        step = step_dist(vx), step_dist(vy)
+        return move(tail, step)
+
+
+def transition(initial: State, instruction: Instruction) -> Iterator[State]:
+    direction, distance = instruction
+    step = STEP_VECTORS[direction]
+    head, tail = initial
+    for _ in range(distance):
+        head = move(head, step)
+        tail = catch_up(head, tail)
+        yield head, tail
+
+
+def all_transitions(
+    initial: State, instructions: Iterable[Instruction]
+) -> Iterator[State]:
+    print_(initial)
+    yield initial
+    for instruction in instructions:
+        state = initial
+        for state in transition(initial, instruction):
+            print_(state)
+            yield state
+        initial = state
+
+
+def run(input_: IO[str], verbose: bool = False):
+    set_verbose(verbose)
+    instructions = list(map(parse_instruction, input_))
+    initial = (Coord(0), Coord(0)), (Coord(0), Coord(0))
+    states = all_transitions(initial, instructions)
+    return len(set(map(itemgetter(1), states)))
+
+
+test_input = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
+
+
+def test():
+    import io
+
+    actual = run(io.StringIO(test_input))
+    expected = 13
+    assert actual == expected, (actual, expected)
