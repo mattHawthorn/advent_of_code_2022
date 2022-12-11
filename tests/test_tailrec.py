2b23fc7854f613937381ff7f2000a9d6165e1f12
matt.hawthorn <matt.hawthorn@trillianthealth.com>
2022-12-11 12:20:51 -0500

    clarifying comments

--- tests/test_tailrec.py
+++ tests/test_tailrec.py
@@ -17,13 +17,15 @@ def factorial(n: int, acc: int = 1, stack_height: int = 0) -> Tuple[int, int]:
 
 
 def recursive_function_overwrites_default(x: int, y: str, default: int = 0) -> int:
-    s = str(x)
     length = len(y) // 2
     if not (x > 30 or len(y) > 20):
-        default = 1
+        default = 1  # overwrite; ensure it's reset correctly on loop
         print("recurse")
-        return recursive_function_overwrites_default(y=y + s, x=x + length + default)
+        return recursive_function_overwrites_default(
+            y=y + str(x), x=x + length + default
+        )
     else:
+        # using it here ensures failure if it wasn't set correctly on loop
         return x + length + default
 
 
