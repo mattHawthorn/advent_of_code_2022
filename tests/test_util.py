a0846f5e9bbd759925e24d387e1bd0d207fbdf52
matt.hawthorn <matt.hawthorn@trillianthealth.com>
2023-09-08 04:31:36 -0400

    add tests for reduce_by

---
+++ tests/test_util.py
@@ -0,0 +1,33 @@
+import operator
+
+import pytest
+
+from solutions import util
+
+
+def is_inc(a, b):
+    return b - a == 1
+
+
+def longest_common_prefix(s1, s2):
+    return "".join(a for a, b in zip(s1, s2) if a == b)
+
+
+def share_prefix(s1, s2):
+    return bool(s1) and bool(s2) and s1[0] == s2[0]
+
+
+def same_parity(a, b):
+    return (b - a) % 2 == 0
+
+
+@pytest.mark.parametrize(
+    "op, agg, values, init, expected",
+    [
+        (same_parity, operator.add, [1, 2, 3, 1, 5], None, [1, 2, 4, 5]),
+        (operator.eq, operator.add, [0, 0, 0, 1, 1, 2, 4], None, [0, 8]),
+    ],
+)
+def test_reduce_while(op, agg, values, init, expected):
+    actual = list(util.reduce_while(op, agg, values, init))
+    assert expected == actual, (expected, actual)
